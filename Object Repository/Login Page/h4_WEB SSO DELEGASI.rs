c4e80b1fa4e31a75992940edee5820f3240ed813
meidinawh <55943139+meidinawh@users.noreply.github.com>
2023-05-29 11:02:52 +0700

    first commit

---
+++ Object Repository/Login Page/h4_WEB SSO DELEGASI.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h4_WEB SSO DELEGASI</name>
+   <tag></tag>
+   <elementGuidId>dd5701af-29a8-4065-ad1c-bec40980ead8</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>h4.title-head.mx-2</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='root']/div[2]/div/div/div/h4</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h4</value>
+      <webElementGuid>89bb1b1c-8820-4e05-aa19-a0eeea892a6d</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>title-head mx-2</value>
+      <webElementGuid>d8024274-8480-4f3e-9ba6-a543bcb60809</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>WEB SSO DELEGASI</value>
+      <webElementGuid>1eb943c2-31cf-482c-9e54-d3d671c34129</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[@class=&quot;d-flex&quot;]/div[@class=&quot;m-0 p-0 container&quot;]/div[@class=&quot;mt-3 p-0 container&quot;]/div[@class=&quot;wrapper-headnav&quot;]/h4[@class=&quot;title-head mx-2&quot;]</value>
+      <webElementGuid>5a36a340-cab3-4214-bf2f-5464e2635618</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div[2]/div/div/div/h4</value>
+      <webElementGuid>3f855545-e583-47b0-b9e4-0eda8129677c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='PERSETUJUAN'])[1]/following::h4[1]</value>
+      <webElementGuid>06d2edb4-2f6c-4565-a573-1cda7bbb8209</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Pergantian'])[1]/following::h4[1]</value>
+      <webElementGuid>12e0ca43-4cfd-4bd5-849e-9aa452513630</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Cari Data Pendaftaran User Backup'])[1]/preceding::h4[1]</value>
+      <webElementGuid>857b7c62-db24-4b9d-869b-1c55b0a6b12e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='NIK'])[1]/preceding::h4[1]</value>
+      <webElementGuid>de108c1a-d9ab-40ed-9182-199125dcc621</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='WEB SSO DELEGASI']/parent::*</value>
+      <webElementGuid>013e12a1-81d8-45e6-a406-b6252e955eab</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//h4</value>
+      <webElementGuid>8f008ea2-af5a-47b8-b124-41e2844c0a1b</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h4[(text() = 'WEB SSO DELEGASI' or . = 'WEB SSO DELEGASI')]</value>
+      <webElementGuid>81afcd0c-5c2d-4645-85e7-16f859107680</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
---
+++ Object Repository/Search Page/button_Cari.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Cari</name>
+   <tag></tag>
+   <elementGuidId>985acfdd-7685-42c7-b3be-ec723eaa2554</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>button.mt-3.btn-color</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='root']/div[2]/div/div[2]/div/div/div/button</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>684170dc-94b2-4412-a132-623f365bf1fe</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>mt-3 btn-color</value>
+      <webElementGuid>2e16f038-3c59-4eb4-af66-2e94ce38d640</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Cari</value>
+      <webElementGuid>c4ba33fb-95f7-4f76-ae99-4f8621dd7d8b</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/div[@class=&quot;d-flex&quot;]/div[@class=&quot;m-0 p-0 container&quot;]/div[@class=&quot;mt-3 mx-auto&quot;]/div[@class=&quot;px-3&quot;]/div[@class=&quot;mx-auto card border-secondary&quot;]/div[@class=&quot;card-body&quot;]/button[@class=&quot;mt-3 btn-color&quot;]</value>
+      <webElementGuid>869d0cf8-13c4-4d8f-9e32-227356722c15</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/div[2]/div/div[2]/div/div/div/button</value>
+      <webElementGuid>3f4b69c5-251f-474c-a133-c5e5be38612c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='NIK'])[1]/following::button[1]</value>
+      <webElementGuid>c467f280-d7cb-489e-87db-33814435b04d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Cari Data Pendaftaran User Backup'])[1]/following::button[1]</value>
+      <webElementGuid>f6f23e20-6321-421b-b166-c104075532a8</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Hasil Pencarian'])[1]/preceding::button[1]</value>
+      <webElementGuid>34681c66-ee52-43ee-bc7b-dd6af12b6937</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='NIK'])[2]/preceding::button[1]</value>
+      <webElementGuid>986835be-fdc7-4c93-a6f4-2ef54e49941c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Cari']/parent::*</value>
+      <webElementGuid>1c129900-90e6-4a19-9e1a-375c35ce6198</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/button</value>
+      <webElementGuid>813d6fc6-65c4-4065-88c4-dbff935692a7</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[(text() = 'Cari' or . = 'Cari')]</value>
+      <webElementGuid>26e11a0f-4d40-4d8f-8c6d-9e605cfbdd3c</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
